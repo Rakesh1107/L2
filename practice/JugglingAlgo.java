eaa5ee8ec3bfa8355f16b447886262e757e22922
Rakesh1107 <mragesh1998@gmail.com>
2021-08-27 16:11:12 +0530

    changes made

--- practice/JugglingAlgo.java
+++ practice/JugglingAlgo.java
@@ -1,13 +1,41 @@
 package practice;
 
+import java.util.Arrays;
+
+import static practice.GCD.gcd;
+
 public class JugglingAlgo {
     public static void main(String[] args) {
         int[] array = {2, 4, 5, 1, 6};
-        rotateArray(array);
+        int d = 3;
+        rotateArray(array, d);
+        System.out.println(Arrays.toString(array));
     }
 
-    private static void rotateArray(int[] array) {
+    private static void rotateArray(int[] array, int d) {
+        int n = array.length;
 
+        d = d%n;
+        int _gcd = gcd(d, n);
+
+        int i, j, k, temp;
+        for (i = 0; i < _gcd; i++) {
+            temp = array[i];
+            j = i;
+            while (true) {
+                k = j + d;
+
+                if (k >= n) {
+                    k-=n;
+                }
+                if (k == i) {
+                    break;
+                }
+                array[j] = array[k];
+                j = k;
+            }
+            array[j] = temp;
+        }
 
     }
 
